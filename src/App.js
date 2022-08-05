a5bcbd5d8ad8a9bc6c34af3a238e501fda497953
Jorge Luis Márquez León <jmarquezl1200@alumno.ipn.mx>
2022-08-05 15:24:11 -0500

    Volviendo al error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import SignUp from "./views/SignUp";
 import Forgot from "./views/Forgot";
-import Change from "./views/Change";
 import Dashboard from "./views/Dashboard";
-
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/forgot" element={<Forgot />}></Route>
-        <Route path="/change" element={<Change />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </div>
