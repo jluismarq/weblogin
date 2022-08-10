12f0d50c4dc43efeda106a8491b8fc695fa94833
Jorge Luis Márquez León <jmarquezl1200@alumno.ipn.mx>
2022-08-10 12:36:27 -0500

    entities

--- src/entities/users.js
+++ src/entities/users.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const token=(usuario)=>{
+    return axios({
+        url:'usuarios/token/obtener/',
+        method:'POST',
+        baseURL: 'https://apiskydelight.herokuapp.com/',
+        data:usuario,
+    })
+}
+
+
 const crearUsuario=(usuario)=>{
    return axios({
                 url: 'usuarios/crearusuario/',
@@ -9,32 +19,35 @@ const crearUsuario=(usuario)=>{
                 data: usuario,
             }
         )
-    }
+}
 
-    const recuperarPassword=(usuario)=>{
-        return axios({
-                     url: 'usuarios/recuperar-contraseña/',
-                     method: 'POST', 
-                     baseURL: 'https://apiskydelight.herokuapp.com/',
-                     headers: {"KEY-CLIENT":process.env.REACT_APP_API_KEY},
-                     data: usuario,
-                 }
-         )
-     }
+const recuperarPassword=(usuario)=>{
+    return axios({
+                url: 'usuarios/recuperar-contraseña/',
+                method: 'POST', 
+                baseURL: 'https://apiskydelight.herokuapp.com/',
+                headers: {"KEY-CLIENT":process.env.REACT_APP_API_KEY},
+                data: usuario,
+            }
+        )
+}
     
-     const cambiarPassword=(usuario)=>{
-        return axios({
-                     url: 'usuarios/cambiar-contrasena/',
-                     method: 'PUT', 
-                     baseURL: 'https://apiskydelight.herokuapp.com/',
-                     headers: {"KEY-CLIENT":process.env.REACT_APP_API_KEY},
-                     data: usuario,
-                 }
-         )
-     }
+const cambiarPassword=(usuario)=>{
+    return axios({
+                url: 'usuarios/cambiar-contrasena/',
+                method: 'PUT', 
+                baseURL: 'https://apiskydelight.herokuapp.com/',
+                headers: {"KEY-CLIENT":process.env.REACT_APP_API_KEY},
+                data: usuario,
+                }
+        )
+}
+
+
     
     export{
         crearUsuario,
         recuperarPassword,
-        cambiarPassword
+        cambiarPassword,
+        token
     }
