71e027674ddbe1aa804c8e56840f1da80d94e817
Jorge Luis Márquez León <jmarquezl1200@alumno.ipn.mx>
2022-09-09 14:24:08 -0500

    mensaje de informacion no disponible

--- src/components/PSS.js
+++ src/components/PSS.js
@@ -1,63 +1,24 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Title from '../Dashcomponents/Title';
-import TableContainer from '@mui/material/TableContainer';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Title from "../Dashcomponents/Title";
+import TableContainer from "@mui/material/TableContainer";
 import { useAuth } from "../hooks/useAuth";
 import { obtenerPSS } from "../entities/questionnarie";
+import { Typography } from "@mui/material";
 
 function createData(jsonResponse) {
- return jsonResponse.data.data.map(Test => { 
-  const {usuario,created_at,total}=Test;
-  const preguntas = Object.keys(Test)
-  .filter(key => key.includes("pregunta"))
-  .map(key=>({key:Test[key]}))
-  return{usuario,created_at,total,preguntas}
-});  
+  return jsonResponse.data.data.map((Test) => {
+    const { usuario, created_at, total } = Test;
+    const preguntas = Object.keys(Test)
+      .filter((key) => key.includes("pregunta"))
+      .map((key) => ({ key: Test[key] }));
+    return { usuario, created_at, total, preguntas };
+  });
 }
-
-// const rows = [
-//   createData(
-//     0,
-//     '16 Mar, 2022',
-//     '12',
-//     '7',
-//     '8',
-//     27,
-//   ),
-//   createData(
-//     1,
-//     '18 Mar, 2022',
-//     '11',
-//     '12',
-//     '15',
-//     38,
-//   ),
-//   createData(2, '20 Mar, 2022', '8', '9', '10', 27),
-//   createData(
-//     3,
-//     '22 Mar, 2022',
-//     '10',
-//     '14',
-//     '13',
-//     37,
-//   ),
-//   createData(
-//     4,
-//     '24 Mar, 2022',
-//     '9',
-//     '8',
-//     '7',
-//     24,
-//   ),
-// ];
-
-// function preventDefault(event) {
-//   event.preventDefault();
-// }
 
 export default function Test() {
   const auth = useAuth();
@@ -69,7 +30,7 @@ export default function Test() {
         email: auth.user.user,
         access: auth.user.access,
       }).then((jsonResponse) => {
-        console.log(jsonResponse);
+        // console.log(jsonResponse);
         setTest(createData(jsonResponse));
       });
     };
@@ -78,43 +39,47 @@ export default function Test() {
 
   return (
     <React.Fragment>
-      <Title>Escala de Estrés Percibido</Title>
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Fecha</TableCell>
-              {/* {console.log(test)} */}
-              {test[0] &&
-                test[0].preguntas.map((pregunta, index) => {
-                  return <TableCell>Pregunta {index + 1}</TableCell>;
-                })}
-              <TableCell align="right">Total</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {test &&
-              test.map((row) => (
-                <TableRow
-                  key={row.created_at}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell>
-                    {new Date(row.created_at).toLocaleDateString()}
-                  </TableCell>
-                  {row.preguntas.map((pregunta, index) => {
-                    return (
-                      <TableCell>
-                        {pregunta[Object.keys(pregunta)[0]]}
-                      </TableCell>
-                    );
+      <Title>Lista de Indicadores de Vulnerabilidad al Estrés</Title>
+      {!(test.length === 0) ? (
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Fecha</TableCell>
+                {/* {console.log(test)} */}
+                {test[0] &&
+                  test[0].preguntas.map((pregunta, index) => {
+                    return <TableCell>Pregunta {index + 1}</TableCell>;
                   })}
-                  <TableCell align="right">{row.total}</TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+                <TableCell align="right">Total</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {test &&
+                test.map((row) => (
+                  <TableRow
+                    key={row.created_at}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>
+                      {new Date(row.created_at).toLocaleDateString()}
+                    </TableCell>
+                    {row.preguntas.map((pregunta, index) => {
+                      return (
+                        <TableCell>
+                          {pregunta[Object.keys(pregunta)[0]]}
+                        </TableCell>
+                      );
+                    })}
+                    <TableCell align="right">{row.total}</TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Typography>No hay información disponible</Typography>
+      )}
     </React.Fragment>
   );
 }
